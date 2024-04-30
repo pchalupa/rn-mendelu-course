57db6afb2e2f4c6d74f194016a0f654a938d32da
pchalupa <chalupa.petr93@gmail.com>
2024-04-30 12:27:24 +0200

    chore: cleanup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,4 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
-
-import { useColorScheme } from '~/components/useColorScheme';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -14,18 +11,10 @@ export const unstable_settings = {
 };
 
 export default function RootLayout() {
-  return <RootLayoutNav />;
-}
-
-function RootLayoutNav() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+    </Stack>
   );
 }
