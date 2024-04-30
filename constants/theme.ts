d2a996c4aa13d4ce501f54f2103f79c84d128a3a
pchalupa <chalupa.petr93@gmail.com>
2024-04-30 12:30:39 +0200

    feat: theme

---
+++ constants/theme.ts
@@ -0,0 +1,9 @@
+export const colors = {
+  primary: '#333D29',
+  secondary: '#43281C',
+  ternary: '#7F4F24',
+  alternative1: '#B08968',
+  alternative2: '#DDB892',
+  cardBackground: '#EDE0D4',
+  surfaceBackground: '#EFEFEF',
+};
